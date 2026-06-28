f0ee541cf204513e3da5735827f67d10c646e5da
Vishvendra-ux <pratapsinghvishvendra6@gmail.com>
2026-06-28 14:02:37 +0530

    Primitive AND non primitive

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -9,30 +9,54 @@
 // 6. Null
 // 7. Symbol
 
-const score = 100;
-const scoreValue = 100.3;
-const isLoggedIn = false;
-const outsideTemp = null;
-let userEmail;
+// const score = 100;
+// const scoreValue = 100.3;
+// const isLoggedIn = false;
+// const outsideTemp = null;
+// let userEmail;
 
-const id = Symbol('123');
-const anotherId = Symbol('123');
-console.log(id === anotherId);
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+// console.log(id === anotherId);
 
 
 // Non-Primitive
 // Array, Object, Functions 
 
-const heros = ['shaktiman', 'naagraj', 'doga'];
-let myObj = {
-  name: "Vishvendra",
-  age: 21,
+// const heros = ['shaktiman', 'naagraj', 'doga'];
+// let myObj = {
+//   name: "Vishvendra",
+//   age: 21,
+// }
+
+// const myFunction = function() {
+//   console.log("Hello World");
+// }
+
+// console.log(typeof bigNumber);
+
+// console.log(typeof outsideTemp);
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive) and Heap (Non-Primitive) memory allocation
+
+let myYoutubeName = "Vishvendra";
+
+let anothername = myYoutubeName;
+anothername = "Codevolution";
+
+console.log(myYoutubeName);
+console.log(anothername);
+
+let userOne = {
+  email: "vishvendra@example.com",
+  upi: "vishvendra@ybl"
 }
 
-const myFunction = function() {
-  console.log("Hello World");
-}
+let userTwo = userOne;
+userTwo.email = "userTwo@example.com";
 
-console.log(typeof bigNumber);
-
-console.log(typeof outsideTemp);
+console.log(userOne.email);
+console.log(userTwo.email);
