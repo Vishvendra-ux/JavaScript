f36726eff495fcfea7b8c02976ee64fbd02df1e0
Vishvendra-ux <pratapsinghvishvendra6@gmail.com>
2026-06-28 10:32:33 +0530

    Add 01_Basics/02_Datatypes.js

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let accountEmail = "Vishvendra@google.com";
+var accountPassword = "123455";
+accountCity = "Jaipur";
+
+//accountId=2; not allowed
+
+accountEmail = "Pratap@google.com";
+accountPassword = "123456";
+accountCity = "Mumbai";
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+//LECTURE 5---->
+"use strict"; //treat all js code as newer version
+
+// alert(3+3); we are using node js not browser so alert will not work
+
+console.log(3+3); // this will work in node js. code readability should be high
+console.log("Vishvendra"); // this will work in node js
+
+let name = "Vishvendra"; //string literal
+let age = 22;
+let isLoggedIn = false; //boolean literal
+
+//number => 2 to the power of 53
+// bigint => larger than 2 to the power of 53
+// string => sequence of characters
+// boolean => true or false
+// null => standalone value
+//undefined => value taken by a variable that is not yet defined
+//symbol => unique identifiers for objects
+
+//object => a collection of key value pairs
+
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
