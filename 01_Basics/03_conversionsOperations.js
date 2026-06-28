dc8de7fbd9f1fd4d1d2645e7ceebec690294d39e
Vishvendra-ux <pratapsinghvishvendra6@gmail.com>
2026-06-28 13:23:50 +0530

    Add conversions and operations examples

--- 01_Basics/03_conversionsOperations.js
+++ 01_Basics/03_conversionsOperations.js
@@ -21,7 +21,40 @@
 //"" => false; "abc" => true
 //"Vishvendra" => true
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************** Operations ***********************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(3 + 2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(3%2);
+// console.log(2/3); // remainder
+
+// let str1 = "Vishvendra";
+// let str2 = " Singh";
+// let str3 = str1 + str2;
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
