c1d043d94f117a4a0a2eb6b5d227a166aa04e777
Vishvendra-ux <pratapsinghvishvendra6@gmail.com>
2026-06-30 18:02:15 +0530

    DateTime

---
+++ 01_Basics/07_date.js
@@ -0,0 +1,15 @@
+// Dates
+
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate); // object
+
+// let myDate2 = new Date('8-4-2003');
+// console.log(myDate2.toDateString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp); // 1696540800000
+
+console.log(Math.floor(Date.now() / 1000)); // 1696540800
